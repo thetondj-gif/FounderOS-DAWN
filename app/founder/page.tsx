7ffc68e1221540b3eb3aa32133e076b8eb2139f6
thetondj-gif <the.ton.dj@googlemail.com>
2026-09-06 13:39:37 +0100

    feat: add mobile founder command center page

---
+++ app/founder/page.tsx
@@ -0,0 +1,7 @@
+import { FounderCommandCenter } from '@/components/FounderCommandCenter';
+
+export const dynamic = 'force-dynamic';
+
+export default function FounderPage() {
+  return <FounderCommandCenter />;
+}
